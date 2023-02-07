7fdcb5fde07b3197ffb3029d5f35c1f8530f507b
Brandon Villalobos <122856474+Fellmouse@users.noreply.github.com>
2023-02-07 09:57:23 -0800

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,56 @@
+"""
+print numbers 1-150
+
+number = 1
+while number <= 150:
+  print(number)
+  number = number + 1
+"""
+
+'''
+print all numbers divisable by 5
+
+for number in range(1,1001):
+  if number % 5 == 0:
+    print(number)
+'''
+
+'''
+if divisible by 5 or 10
+
+for number in range(1, 101):
+  if number % 5 == 0 and number % 10 == 0:
+    print("Coding Dojo")
+  elif number % 5 == 0:
+    print("Coding")
+'''
+
+'''
+sum off all odd numbers entered
+
+n= int(input("enter number: "))
+sum=0
+for i in range(1, n + 1):
+  if (i % 2 != 0):
+    sum= sum + i
+print(sum)
+['''
+
+'''
+Counting Backwards by 4
+
+for x in range(2018, 1, -4):
+  print(x)
+'''
+
+'''
+print only the integers that are a multiple of mult
+
+lownum = 2
+highnum = 10
+multi = 3
+
+for x in range(lownum, highnum):
+  if (x % 3 == 0):
+    print(x, end =", ")
+'''
